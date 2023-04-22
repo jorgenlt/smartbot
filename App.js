782f761e34c21a00709512f9dff3ba5d4ec6a3dc
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-22 21:25:33 +0200

    text input

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Is it working?</Text>
-      <Text>Hello</Text>
+      <Text style={{color: 'white'}} >Open up App.js to start sartworking on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <View style={styles.inputWrapper}>
+          <TextInput
+          style={styles.input}
+          />
+          <Button 
+            title='send'
+            color='transparent'
+            style={styles.sendBtn} 
+            onPress={() => console.log('send message')}
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
-    backgroundColor: 'purple',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#121416',
+    justifyContent: 'flex-end',
+    paddingVertical: 10,
+    paddingHorizontal: 5
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    marginStart: 5,
+    marginEnd: 5,
+    height: 40,
+    // width: '80%',
+    borderColor: '#d2d4da',
+    borderWidth: 1,
+    borderRadius: 20,
+    color: '#f5f5f5',
+  },
+  sendBtn: {
   }
 });
