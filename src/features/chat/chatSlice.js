e597caa50514967c03eb320cb025154ac5cec677
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-12-27 14:32:41 +0800

    update anthropic models

--- src/features/chat/chatSlice.js
+++ src/features/chat/chatSlice.js
@@ -19,10 +19,10 @@ const initialState = {
     anthropic: {
       name: "Anthropic",
       key: null,
-      model: "claude-3-sonnet-20240229",
+      model: "claude-3-5-sonnet-20241022",
       models: [
-        "claude-3-haiku-20240307",
-        "claude-3-5-sonnet-20240620",
+        "claude-3-5-haiku-20241022",
+        "claude-3-5-sonnet-20241022",
         "claude-3-opus-20240229",
       ],
     },
