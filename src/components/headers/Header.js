5e8de2127d473d9454eb95656b4ea3afe31bcde9
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-12-31 13:48:13 +0800

    header component

--- src/components/headers/Header.js
+++ src/components/headers/Header.js
@@ -1,36 +1,38 @@
 import { Text, StyleSheet, View } from "react-native";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 import { colors } from "../../styles/colors";
 
-const Header = () => {
+const ChatHeader = () => {
+  const theme = useSelector((state) => state.chat.theme);
+  const styles = useMemo(() => styling(theme), [theme]);
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>ChatHeader</Text>
-      </View>
-      <View>
-        <Text style={styles.text}>Share</Text>
+        <Text style={styles.text}>Chat</Text>
       </View>
     </View>
   );
 };
 
-export default Header;
+export default ChatHeader;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    maxHeight: 80,
-    paddingTop: 20,
-    paddingLeft: 20,
-    paddingRight: 10,
-    width: "100%",
-    backgroundColor: colors.light.black,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  text: {
-    color: "white",
-    fontSize: 20,
-  },
-});
+const styling = (theme) =>
+  StyleSheet.create({
+    container: {
+      height: 80,
+      paddingTop: 20,
+      paddingLeft: 20,
+      paddingRight: 20,
+      backgroundColor: colors[theme].black,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+    text: {
+      color: colors[theme].white,
+      fontSize: 20,
+      fontWeight: "500",
+    },
+  });
