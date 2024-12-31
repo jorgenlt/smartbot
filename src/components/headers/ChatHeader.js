f8a4168eb5af3c089139a3dcb4a4d4b483d718b0
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-12-31 13:46:22 +0800

    chatheader

--- src/components/headers/ChatHeader.js
+++ src/components/headers/ChatHeader.js
@@ -1,14 +1,20 @@
 import { Text, StyleSheet, View } from "react-native";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 import { colors } from "../../styles/colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const ChatHeader = () => {
+  const theme = useSelector((state) => state.chat.theme);
+  const styles = useMemo(() => styling(theme), [theme]);
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>ChatHeader</Text>
+        <Text style={styles.text}>Chat</Text>
       </View>
       <View>
-        <Text style={styles.text}>Share</Text>
+        <Ionicons name="share-social" size={24} color="white" />
       </View>
     </View>
   );
@@ -16,21 +22,21 @@ const ChatHeader = () => {
 
 export default ChatHeader;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    maxHeight: 80,
-    paddingTop: 20,
-    paddingLeft: 20,
-    paddingRight: 10,
-    width: "100%",
-    backgroundColor: colors.light.black,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  text: {
-    color: "white",
-    fontSize: 20,
-  },
-});
+const styling = (theme) =>
+  StyleSheet.create({
+    container: {
+      height: 80,
+      paddingTop: 20,
+      paddingLeft: 20,
+      paddingRight: 20,
+      backgroundColor: colors[theme].black,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+    text: {
+      color: colors[theme].white,
+      fontSize: 20,
+      fontWeight: "500",
+    },
+  });
